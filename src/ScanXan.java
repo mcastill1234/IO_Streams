eb901359a7f7c39a1cdbaea9a35c36e83a67b705
mcastill1234 <30333120+mcastill1234@users.noreply.github.com>
2018-12-10 11:54:41 -0500

    Initial commit

---
+++ src/ScanXan.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ScanXan {
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("xanadu.txt")));
+            s.useDelimiter(",\\s*");
+
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+}
